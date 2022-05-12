48a7b57c16a2404261f69d953710e6297a742337
ninjaman255 <aaronclevette@gmail.com>
2022-05-12 17:50:58 -0500

    added assets, fixed up the map abit
    same as last commit but with added assets as well as messed around with layering but it didnt work quite right will proceed forward with secret area warp plan laid out 2 commits down from here

---
+++ assets/tiles/BN3UndernetWarpVariant2.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="BN3UndernetWarpVariant2" tilewidth="64" tileheight="40" tilecount="9" columns="1" objectalignment="top">
+ <tileoffset x="0" y="-6"/>
+ <image source="BN3UndernetWarpVariant2.png" trans="000000" width="64" height="360"/>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="11" y="14" width="42" height="21">
+    <ellipse/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="1" duration="166"/>
+   <frame tileid="2" duration="166"/>
+   <frame tileid="3" duration="166"/>
+   <frame tileid="4" duration="166"/>
+   <frame tileid="5" duration="166"/>
+   <frame tileid="6" duration="166"/>
+   <frame tileid="7" duration="166"/>
+   <frame tileid="8" duration="166"/>
+  </animation>
+ </tile>
+</tileset>
