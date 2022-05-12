409f11b3f0ab2a2053bf80d28bc0817938fd8ac0
ninjaman255 <aaronclevette@gmail.com>
2022-05-12 17:18:34 -0500

    updated undernet square
    still needs work,
    
    warps arent animated yet, also need to figure out exactly how i want to link to secret net,
    
    see last commits description for a reminder

---
+++ assets/tiles/Undernet_SquareF2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Undernet_SquareF2" tilewidth="429" tileheight="301" tilecount="1" columns="1" objectalignment="top">
+ <image source="Undernet_SquareF2.png" width="429" height="301"/>
+</tileset>
