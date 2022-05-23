6f9e3f20c893de0fa0b48da862d812e862d3eba4
ninjaman255 <aaronclevette@gmail.com>
2022-05-23 04:10:57 -0500

    added dev stuff grab mystery data and hide the stuff and things

---
+++ assets/tiles/SpecialBlockerObject.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="SpecialBlockerObject" tilewidth="64" tileheight="32" tilecount="10" columns="1">
+ <image source="SpecialBlockerObject.png" width="64" height="320"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="16">
+    <polygon points="0,0 32,-16 64,0 32,16 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
