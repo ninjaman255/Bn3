1d3c43ee6e161d90248255f641a7fdf5a250608c
ninjaman255 <aaronclevette@gmail.com>
2022-05-22 23:26:38 -0500

    Tons of changes
    -swapped majority of warps using old warp script to ezwarps scripts
    -did some basic map work towards alpha liberation missions
    -fixed merch quest
    -added alpha (fightable) and other npcs to setup for cutscene event

---
+++ assets/tiles/CusBeachWarp.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="CusBeachWarp" tilewidth="52" tileheight="34" tilecount="4" columns="4" objectalignment="top">
+ <tileoffset x="-1" y="12"/>
+ <image source="CusBeachWarp.png" width="208" height="34"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="250"/>
+   <frame tileid="1" duration="250"/>
+   <frame tileid="2" duration="250"/>
+   <frame tileid="3" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
--- assets/tiles/shotcutwarps.tsx
+++ assets/tiles/shotcutwarps.tsx
@@ -1,4 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="shotcutwarps" tilewidth="46" tileheight="32" tilecount="1" columns="1" objectalignment="top">
+<tileset version="1.8" tiledversion="1.8.4" name="shotcutwarps" tilewidth="46" tileheight="32" tilecount="1" columns="1" objectalignment="top">
  <image source="shotcutwarps.png" width="46" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="10" y="8" width="26" height="14">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
