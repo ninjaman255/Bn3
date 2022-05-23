18d3014db21e066b51a2f4063929378cef2137fc
ninjaman255 <aaronclevette@gmail.com>
2022-05-23 01:04:19 -0500

    Update alot of undernet
    -undernet 4 warps need to be swapped over to ezwarps
    -added animated conveyors for undernet
    -added arrow typed tiles to show arrows on minimap

---
+++ assets/tiles/Bn3UndernetFlashingWarpVariant2.tsx
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Bn3UndernetFlashingWarpVariant2" tilewidth="44" tileheight="22" tilecount="6" columns="6" objectalignment="top">
+ <tileoffset x="0" y="5"/>
+ <image source="Bn3UndernetFlashingWarpVariant2.png" width="264" height="22"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="5" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
---
+++ assets/tiles/UndernetConveyorAnim.tsx
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="UndernetConveyorAnim" tilewidth="64" tileheight="32" tilecount="16" columns="4" objectalignment="top">
+ <tileoffset x="0" y="-2"/>
+ <image source="UndernetConveyorAnim.png" width="256" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="166"/>
+   <frame tileid="1" duration="166"/>
+   <frame tileid="2" duration="166"/>
+   <frame tileid="3" duration="166"/>
+   <frame tileid="0" duration="166"/>
+   <frame tileid="1" duration="166"/>
+   <frame tileid="2" duration="166"/>
+   <frame tileid="3" duration="166"/>
+   <frame tileid="0" duration="166"/>
+   <frame tileid="1" duration="166"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="166"/>
+   <frame tileid="5" duration="166"/>
+   <frame tileid="6" duration="166"/>
+   <frame tileid="7" duration="166"/>
+   <frame tileid="4" duration="166"/>
+   <frame tileid="5" duration="166"/>
+   <frame tileid="6" duration="166"/>
+   <frame tileid="7" duration="166"/>
+   <frame tileid="4" duration="166"/>
+   <frame tileid="5" duration="166"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="166"/>
+   <frame tileid="9" duration="166"/>
+   <frame tileid="10" duration="166"/>
+   <frame tileid="11" duration="166"/>
+   <frame tileid="8" duration="166"/>
+   <frame tileid="9" duration="166"/>
+   <frame tileid="10" duration="166"/>
+   <frame tileid="11" duration="166"/>
+   <frame tileid="8" duration="166"/>
+   <frame tileid="9" duration="166"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="166"/>
+   <frame tileid="13" duration="166"/>
+   <frame tileid="14" duration="166"/>
+   <frame tileid="15" duration="166"/>
+   <frame tileid="12" duration="166"/>
+   <frame tileid="13" duration="166"/>
+   <frame tileid="14" duration="166"/>
+   <frame tileid="15" duration="166"/>
+   <frame tileid="12" duration="166"/>
+   <frame tileid="13" duration="166"/>
+  </animation>
+ </tile>
+</tileset>
