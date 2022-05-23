5216791bdbb2b42206a9aa2716b8a29f075499d8
ninjaman255 <aaronclevette@gmail.com>
2022-05-22 23:54:44 -0500

    slowly updating undernet area
    -undernet 3, 5, and square have appropriate ezwarps warps
    -added special warp that will serve as the secret net warp

---
+++ assets/tiles/Bn3UndernetFlashingWarp.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Bn3UndernetFlashingWarp" tilewidth="60" tileheight="29" tilecount="6" columns="6" objectalignment="top">
+ <tileoffset x="0" y="2"/>
+ <image source="Bn3UndernetFlashingWarp.png" width="360" height="29"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="2" y="13">
+    <polygon points="0,0 24,-12 32,-12 56,0 56,2 30,15 26,15 0,2"/>
+   </object>
+  </objectgroup>
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="5" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
