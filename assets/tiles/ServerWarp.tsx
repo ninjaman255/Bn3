1d8fb5ac334291f58187632988cd312ded7a376d
ninjaman255 <aaronclevette@gmail.com>
2022-05-23 12:52:53 -0500

    checking to see if different collisions will fix server warp to central server

--- assets/tiles/ServerWarp.tsx
+++ assets/tiles/ServerWarp.tsx
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.0" name="ServerWarp" tilewidth="58" tileheight="30" tilecount="10" columns="5" objectalignment="top">
+<tileset version="1.8" tiledversion="1.8.4" name="ServerWarp" tilewidth="58" tileheight="30" tilecount="10" columns="5" objectalignment="top">
  <tileoffset x="0" y="2"/>
  <image source="ServerWarp.png" trans="ffffff" width="290" height="60"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="5" x="11" y="13">
-    <polygon points="0,0 18,-10 36,0 18,10"/>
+   <object id="6" x="19" y="10" width="20" height="9">
+    <ellipse/>
    </object>
   </objectgroup>
   <animation>
