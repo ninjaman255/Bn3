ec3a928ee858a2f2ac6f7b90d9412148da06bb29
ninjaman255 <aaronclevette@gmail.com>
2022-06-05 17:01:07 -0500

    update

--- scripts/events/eznpcs_events.lua
+++ scripts/events/eznpcs_events.lua
@@ -26,3 +26,44 @@ local StartLiberationAlpha1 ={
     end
 }
 eznpcs.add_event(StartLiberationAlpha1)
+
+local GrantLiberationAbility = {
+    name = "Grant Liberation Mission Ability",
+    action = function(npc, player_id, dialogue, relay_object)
+        return async(function()
+            ezmemory.create_or_update_item("LongSwrd","A Long Sword Chip. Use it in Liberations!",true)
+            local item_count = ezmemory.count_player_item(player_id, "LongSwrd")
+            if item_count > 0 then ezmemory.remove_player_item(player_id, "LongSwrd", item_count) end
+
+            ezmemory.create_or_update_item("WideSwrd","A Wide Sword Chip. Use it in Liberations!",true)
+            item_count = ezmemory.count_player_item(player_id, "WideSwrd")
+            if item_count > 0 then ezmemory.remove_player_item(player_id, "WideSwrd", item_count) end
+
+            ezmemory.create_or_update_item("OldSaber","A Saber projection hilt, scarred with age. Use it in Liberations!",true)
+            item_count = ezmemory.count_player_item(player_id, "OldSaber")
+            if item_count > 0 then ezmemory.remove_player_item(player_id, "OldSaber", item_count) end
+
+            ezmemory.create_or_update_item("HevyShld","A heavy shield, great for defense. Use it in Liberations!",true)
+            item_count = ezmemory.count_player_item(player_id, "HevyShld")
+            if item_count > 0 then ezmemory.remove_player_item(player_id, "HevyShld", item_count) end
+
+            ezmemory.create_or_update_item("HexScyth","A wicked scythe which cleaves most anything. Use it in Liberations!",true)
+            item_count = ezmemory.count_player_item(player_id, "HexScyth")
+            if item_count > 0 then ezmemory.remove_player_item(player_id, "HexScyth", item_count) end
+
+            ezmemory.create_or_update_item("NumGadgt","A gadget that's constantly calculating outcomes. Use it in Liberations!",true)
+            item_count = ezmemory.count_player_item(player_id, "NumGadgt")
+            if item_count > 0 then ezmemory.remove_player_item(player_id, "NumGadgt", item_count) end
+            ezmemory.create_or_update_item("GutsHamr","A hammer that takes guts to wield. Use it in Liberations!",true)
+            item_count = ezmemory.count_player_item(player_id, "GutsHamr")
+            if item_count > 0 then ezmemory.remove_player_item(player_id, "GutsHamr", item_count) end
+
+            ezmemory.create_or_update_item("ShdwShoe","Delicate shoes that let you walk on air. Wear them in Liberations!",true)
+            item_count = ezmemory.count_player_item(player_id, "ShdwShoe")
+            if item_count > 0 then ezmemory.remove_player_item(player_id, "ShdwShoe", item_count) end
+
+            if dialogue.custom_properties["Ability Item"] ~= nil then ezmemory.give_player_item(player_id, dialogue.custom_properties["Ability Item"], 1) end
+        end)
+    end
+}
+eznpcs.add_event(GrantLiberationAbility)
