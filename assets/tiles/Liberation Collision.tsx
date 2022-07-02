4afb43a72acdca392d83fc719997a6dfc25bcec5
ninjaman255 <aaronclevette@gmail.com>
2022-07-02 14:04:11 -0500

    fix liberation add "Liberation Tileset" Custom Property

---
+++ assets/tiles/Liberation Collision.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.4" name="Liberation Collision" tilewidth="50" tileheight="30" tilecount="1" columns="1" objectalignment="top">
+ <tileoffset x="0" y="1"/>
+ <image source="Liberation Collision.png" width="50" height="30"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="25" y="-2">
+    <polygon points="0,0 33,17 0,34 -33,17"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
