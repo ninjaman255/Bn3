257f6da60710089221812b739076d5ccca22af34
ninjaman255 <aaronclevette@gmail.com>
2023-02-23 10:08:53 -0600

    Update ezcheckpoints.lua
    Updated to have password locks, I got it really close and asked dawn for help with fixing the last little bit of the update
    New feature: Password barriers
    -case sensitive
    (will be useful for password doors/gates etc)

--- scripts/ezlibs-custom/ezcheckpoints.lua
+++ scripts/ezlibs-custom/ezcheckpoints.lua
@@ -1,4 +1,5 @@
 local ezmemory = require('scripts/ezlibs-scripts/ezmemory')
+local helpers = require('scripts/ezlibs-scripts/helpers')
 
 local ezcheckpoints = {}
 
@@ -12,16 +13,18 @@ Net:on("object_interaction", function(event)
     if checkpoint_object.type ~= "Checkpoint" then return end
     local required_item = checkpoint_object.custom_properties["Unlock Item"]
     local dialogue_type = checkpoint_object.custom_properties["Dialogue Type"]
+    local password = checkpoint_object.custom_properties["Password"]
     local take_item = checkpoint_object.custom_properties["Take Item"] == "true"
     local dialogue_check = not dialogue_type or dialogue_type and dialogue_type ~= "None"
     if not required_item then return end
     local item_count = 0
+
     if string.lower(required_item) == "money" then
         item_count = Net.get_player_money(player_id)
     else
         item_count = ezmemory.count_player_item(player_id, required_item)
     end
-	local necessary_count = 1
+    local necessary_count = 1
     local is_hide_forever = false
     if checkpoint_object.custom_properties["Once"] ~= nil then
         is_hide_forever = tostring(checkpoint_object.custom_properties["Once"])
@@ -29,7 +32,53 @@ Net:on("object_interaction", function(event)
     if checkpoint_object.custom_properties["Required Keys"] ~= nil then
         necessary_count = tonumber(checkpoint_object.custom_properties["Required Keys"])
     end
-    if item_count >= necessary_count then
+
+    if password ~= nil then
+        return async(function() --You needed to use this to create a promise, you can see how it does so in helpers.
+            local checkpassword = await(Async.prompt_player(player_id))
+            if checkpassword == password then
+                Net.lock_player_input(player_id)
+                local lockedGID = checkpoint_object.custom_properties["Locked GID"]
+                if lockedGID == nil then
+                    Net.set_object_custom_property(area_id, checkpoint_object.id, "Locked GID",
+                        checkpoint_object.data.gid)
+                    lockedGID = checkpoint_object.data.gid
+                end
+                if dialogue_check then
+                    if checkpoint_object.custom_properties["Unlock Message"] ~= nil then
+                        Net.message_player(player_id, checkpoint_object.custom_properties["Unlock Message"])
+                    else
+                        Net.message_player(player_id,
+                            "You unlocked " ..
+                            checkpoint_object.name ..
+                            " with " .. checkpoint_object.custom_properties["Unlock Item"] .. "!")
+                    end
+                end
+                local unlock_data = {
+                    type = "tile",
+                    gid = checkpoint_object.data.gid + checkpoint_object.custom_properties["Unlock GID"],
+                    flipped_horizontally = checkpoint_object.data.flipped_horizontally,
+                    flipped_vertically = checkpoint_object.data.flipped_vertically
+                }
+                Net.set_object_data(area_id, checkpoint_object.id, unlock_data)
+                Async.sleep(tonumber(checkpoint_object.custom_properties["Disappear Cooldown"])).and_then(function()
+                    if is_hide_forever == "true" then
+                        ezmemory.hide_object_from_player(player_id, area_id, object_id) --Hide permanently if needed
+                    else
+                        ezmemory.hide_object_from_player_till_disconnect(player_id, area_id, object_id)
+                    end --Otherwise hide it until disconnect
+                    local relock_data = {
+                        type = "tile",
+                        gid = lockedGID,
+                        flipped_horizontally = checkpoint_object.data.flipped_horizontally,
+                        flipped_vertically = checkpoint_object.data.flipped_vertically
+                    }
+                    Net.set_object_data(area_id, checkpoint_object.id, relock_data)
+                    Net.unlock_player_input(player_id)
+                end)
+            end
+        end)
+    elseif item_count >= necessary_count then
         Net.lock_player_input(player_id)
         local lockedGID = checkpoint_object.custom_properties["Locked GID"]
         if lockedGID == nil then
@@ -40,31 +89,37 @@ Net:on("object_interaction", function(event)
             if checkpoint_object.custom_properties["Unlock Message"] ~= nil then
                 Net.message_player(player_id, checkpoint_object.custom_properties["Unlock Message"])
             else
-                Net.message_player(player_id, "You unlocked "..checkpoint_object.name.." with "..checkpoint_object.custom_properties["Unlock Item"].."!")
+                Net.message_player(player_id,
+                    "You unlocked " ..
+                    checkpoint_object.name .. " with " .. checkpoint_object.custom_properties["Unlock Item"] .. "!")
             end
         end
         local unlock_data = {
             type = "tile",
-            gid=checkpoint_object.data.gid + checkpoint_object.custom_properties["Unlock GID"],
-            flipped_horizontally=checkpoint_object.data.flipped_horizontally,
-            flipped_vertically=checkpoint_object.data.flipped_vertically
+            gid = checkpoint_object.data.gid + checkpoint_object.custom_properties["Unlock GID"],
+            flipped_horizontally = checkpoint_object.data.flipped_horizontally,
+            flipped_vertically = checkpoint_object.data.flipped_vertically
         }
         Net.set_object_data(area_id, checkpoint_object.id, unlock_data)
         Async.sleep(tonumber(checkpoint_object.custom_properties["Disappear Cooldown"])).and_then(function()
-            if is_hide_forever == "true" then ezmemory.hide_object_from_player(player_id,area_id,object_id) --Hide permanently if needed
-            else ezmemory.hide_object_from_player_till_disconnect(player_id,area_id,object_id) end --Otherwise hide it until disconnect
+            if is_hide_forever == "true" then
+                ezmemory.hide_object_from_player(player_id, area_id, object_id) --Hide permanently if needed
+            else
+                ezmemory.hide_object_from_player_till_disconnect(player_id, area_id, object_id)
+            end --Otherwise hide it until disconnect
             local relock_data = {
                 type = "tile",
-                gid=lockedGID,
-                flipped_horizontally=checkpoint_object.data.flipped_horizontally,
-                flipped_vertically=checkpoint_object.data.flipped_vertically
+                gid = lockedGID,
+                flipped_horizontally = checkpoint_object.data.flipped_horizontally,
+                flipped_vertically = checkpoint_object.data.flipped_vertically
             }
             Net.set_object_data(area_id, checkpoint_object.id, relock_data)
             if take_item then
                 if required_item == "money" then
                     ezmemory.spend_player_money(player_id, necessary_count)
                 else
-                    ezmemory.remove_player_item(player_id, checkpoint_object.custom_properties["Unlock Item"], necessary_count)
+                    ezmemory.remove_player_item(player_id, checkpoint_object.custom_properties["Unlock Item"],
+                        necessary_count)
                 end
             end
             Net.unlock_player_input(player_id)
@@ -75,9 +130,13 @@ Net:on("object_interaction", function(event)
                 Net.message_player(player_id, checkpoint_object.custom_properties["Locked Message"])
             else
                 if checkpoint_object.name ~= "" then
-                    Net.message_player(player_id, "You don't have enough "..checkpoint_object.custom_properties["Unlock Item"].." to unlock the "..checkpoint_object.name.."...")
+                    Net.message_player(player_id,
+                        "You don't have enough " ..
+                        checkpoint_object.custom_properties["Unlock Item"] ..
+                        " to unlock the " .. checkpoint_object.name .. "...")
                 else
-                    Net.message_player(player_id, "You don't enough "..checkpoint_object.custom_properties["Unlock Item"].." to unlock this...")
+                    Net.message_player(player_id,
+                        "You don't enough " .. checkpoint_object.custom_properties["Unlock Item"] .. " to unlock this...")
                 end
             end
         end
@@ -87,9 +146,13 @@ Net:on("object_interaction", function(event)
                 Net.message_player(player_id, checkpoint_object.custom_properties["Locked Message"])
             else
                 if checkpoint_object.name ~= "" then
-                    Net.message_player(player_id, "You don't have the "..checkpoint_object.custom_properties["Unlock Item"].." to unlock this "..checkpoint_object.name.."...")
+                    Net.message_player(player_id,
+                        "You don't have the " ..
+                        checkpoint_object.custom_properties["Unlock Item"] ..
+                        " to unlock this " .. checkpoint_object.name .. "...")
                 else
-                    Net.message_player(player_id, "You don't have the "..checkpoint_object.custom_properties["Unlock Item"].." for this...")
+                    Net.message_player(player_id,
+                        "You don't have the " .. checkpoint_object.custom_properties["Unlock Item"] .. " for this...")
                 end
             end
         end
