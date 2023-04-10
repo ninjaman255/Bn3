bda6ac6158bff0ccdfbc357ed1d23198b2ab6c8e
aaronc <aaronclevette@gmail.com>
2023-04-10 12:12:45 -0500

    gate fix

--- assets/tiles/gate.tsx
+++ assets/tiles/gate.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="gate" tilewidth="64" tileheight="52" tilecount="5" columns="1" objectalignment="bottom">
+<tileset version="1.8" tiledversion="1.8.6" name="gate" tilewidth="64" tileheight="52" tilecount="5" columns="1" objectalignment="bottom">
  <tileoffset x="16" y="0"/>
  <grid orientation="orthogonal" width="64" height="32"/>
  <properties>
@@ -7,11 +7,6 @@
  </properties>
  <image source="gate.png" width="64" height="260"/>
  <tile id="0">
-  <objectgroup draworder="index" id="2">
-   <object id="2" x="10" y="36">
-    <polygon points="3,-1 1,-5 7,-5 42,13 43,16 35,16"/>
-   </object>
-  </objectgroup>
   <animation>
    <frame tileid="0" duration="50"/>
    <frame tileid="1" duration="50"/>
